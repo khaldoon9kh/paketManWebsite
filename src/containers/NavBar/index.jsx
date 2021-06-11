5971a0157302cb9a446faa7b513ca00f2c90b636
khaldoon khaldi <khaldoon9kh9@gmail.com>
2021-06-11 17:52:23 +0300

    style NavBar, add fonts

--- src/containers/NavBar/index.jsx
+++ src/containers/NavBar/index.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
-import { Col, Container, Row, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Image } from "react-bootstrap";
+import PaketmanLogo from '../../assests/pics/PaketManWordmark@1X.png';
+import "./index.css"; 
 
 const NavBar = () => {
     return(
-        <Navbar bg="light" expand="lg" sticky="top">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar expand="lg" sticky="top" className="navBarStyle">
+  <Navbar.Brand href="#home">
+      <Image 
+      src={PaketmanLogo} 
+      className="paketManLogo"
+      alt="PaketMan Logo"/>
+  </Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+    <Nav className="mr-auto" id='navBarItems'>
+      <Nav.Link >Features</Nav.Link>
+      <Nav.Link >Join Us</Nav.Link>
+      <Nav.Link >FAQ</Nav.Link>
+      <Nav.Link >Contact Us</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
