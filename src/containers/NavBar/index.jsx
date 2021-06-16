24f08db84ad63f805ddad849efe8f8edbb06dc75
khaldoon khaldi <khaldoon9kh9@gmail.com>
2021-06-16 14:02:05 +0300

    navBar layout fixed

--- src/containers/NavBar/index.jsx
+++ src/containers/NavBar/index.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
-import { Navbar, Nav, Image } from "react-bootstrap";
+import { Navbar, Nav, Image, Container } from "react-bootstrap";
 import PaketmanLogo from '../../assests/pics/PaketManWordmark@1X.png';
 import "./index.css"; 
 
 const NavBar = () => {
     return(
-        <Navbar expand="lg" sticky="top" className="navBarStyle">
-  <Navbar.Brand href="#home">
-      <Image 
-      src={PaketmanLogo} 
-      className="paketManLogo"
-      alt="PaketMan Logo"/>
-  </Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto" id='navBarItems'>
-      <Nav.Link >Features</Nav.Link>
+        <Navbar collapseOnSelect expand="lg" sticky="top" className="navBarStyle" >
+          <Navbar.Brand href="#home">
+            <Image 
+            src={PaketmanLogo} 
+            className="paketManLogo"
+            alt="PaketMan Logo"/>
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Container>
+          <Nav className="mr-auto" id='navBarItems'>
+      <Nav.Link >
+        Features
+        </Nav.Link>
       <Nav.Link >Join Us</Nav.Link>
       <Nav.Link >FAQ</Nav.Link>
       <Nav.Link >Contact Us</Nav.Link>
     </Nav>
+    </Container>
   </Navbar.Collapse>
 </Navbar>
     );
