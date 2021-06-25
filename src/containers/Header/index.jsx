d6aaa6197dfbf5ed3f0556b1a24a5da034aaaa76
khaldoon khaldi <khaldoon9kh9@gmail.com>
2021-06-25 16:53:29 +0300

    add fries Icon

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -3,41 +3,38 @@ import { Row, Col, Image, Container } from "react-bootstrap";
 import "./index.css";
 import MobilePhoto from "../../assests/pics/MobilePhoto.png";
 import YellowCercle from "../../assests/pics/yellow-Cercle.svg";
-import BitMap from '../../assests/Bitmap.svg'
+import Fries from "../../assests/fries.svg";
 
 const Header = () => {
   return (
     <Container fluid className="headerContainer" expand="lg">
-        <Row className="headerMainRow">
-          <Col className="headerTextCol" xs={10} md={6}>
-            <Row className="headerTitleRow">
-              <Col className="headerTitleCol">
-                <h1 className="headerTitle">PaketMan</h1>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <p className="headerBody">
-                  Lorem ipsum dolor sit amet, consectetuer adip- iscing elit,
-                  sed diam nonummy nibh euismod tinci- dunt ut laoreet dolore
-                  magna aliquam erat volut- pat. Ut wisi enim ad minim veniam,
-                  quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-                  ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
-                  dolor in hendrerit in vulputate velit esse molestie consequat,
-                  vel illum dolore eu feugiat nulla facilisis at vero eros et
-                  accumsan et iusto odio dignis
-                </p>
-              </Col>
-            </Row>
+      <Row className="headerMainRow">
+        <Col className="headerTextCol" xs={10} md={6}>
+          <div className="headerTitleCol">
+            <h1 className="headerTitle">PaketMan</h1>
+          </div>
+          <Col>
+            <p className="headerBody">
+              Lorem ipsum dolor sit amet, consectetuer adip- iscing elit, sed
+              diam nonummy nibh euismod tinci- dunt ut laoreet dolore magna
+              aliquam erat volut- pat. Ut wisi enim ad minim veniam, quis
+              nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+              aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
+              in hendrerit in vulputate velit esse molestie consequat, vel illum
+              dolore eu feugiat nulla facilisis at vero eros et accumsan et
+              iusto odio dignis
+            </p>
           </Col>
-          <Col className="headerImg" xs={6} md={4}>
-            <Image
-              src={MobilePhoto}
-              className="mobilePhoto"
-              alt="mobile showing PaketMan App"
-            />
-          </Col>
-        </Row>
+        </Col>
+        {/* <Image className='friesImg' src={Fries}/> */}
+        <Col className="headerImg" xs={6} md={4}>
+          <Image
+            src={MobilePhoto}
+            className="mobilePhoto"
+            alt="mobile showing PaketMan App"
+          />
+        </Col>
+      </Row>
     </Container>
   );
 };
