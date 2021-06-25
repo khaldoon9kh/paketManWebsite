ac1dcacb9a1043500fd9618c9fe783dd99c3d11e
khaldoon khaldi <khaldoon9kh9@gmail.com>
2021-06-25 19:38:32 +0300

    responsive fixed

--- src/containers/FAQ/index.jsx
+++ src/containers/FAQ/index.jsx
@@ -16,12 +16,10 @@ import FAQCard from "../../components/FAQCard";
 const FAQ = () => {
   return (
     <Container fluid className="faqContainer" expand="lg">
-      <Row>
-        <Col className="faqTitle">
-          <h2>FAQ</h2>
-        </Col>
+      <Row className="faqTitle">
+        <h2 className="faqTitleText">FAQ</h2>
       </Row>
-      <Row>
+      <Row className="faqTextCont">
         <Col>
           <Accordion className="accordionCont">
             {faqData.map((question, index) => (
