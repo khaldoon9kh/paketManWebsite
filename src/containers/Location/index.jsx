3a38f01976e44015fd9e747044a12b15e7a3e322
khaldoon khaldi <khaldoon9kh9@gmail.com>
2021-06-26 14:21:27 +0300

    responsive done

--- src/containers/Location/index.jsx
+++ src/containers/Location/index.jsx
@@ -1,47 +1,30 @@
 import React, { useState } from "react";
 import { Row, Col, Image, Container } from "react-bootstrap";
 import "./index.css";
+import infosData from "./info";
 import EmailIcon from "../../assests/pics/email.svg";
 import PinIcon from "../../assests/pics/pinMap.svg";
 import PhoneIcon from "../../assests/pics/phone.svg";
 
 const Location = () => {
+  const InfoCard = ({ icon, text }) => {
+    return (
+      <Row className="infoCardRow">
+        <Col className="iconCol">
+          <Image className="iconImg" src={icon} />
+        </Col>
+        <Col className="textCol">
+          <p>{text}</p>
+        </Col>
+      </Row>
+    );
+  };
   return (
     <Container fluid className="locationCont" expand="lg">
       <Row className="infoCont">
-        <Col className="md-3">
-          <Row>
-            <Col className="iconCol">
-              <Image className="iconImg" src={PinIcon} />
-            </Col>
-            <Col className="textCol">
-              <p>
-                Kayabaşı Mah. Akzambak Sk ADIM İSTANBUL Sitesi B2 Blok Dukkan:
-                T20 Başakşehir - İstanbul
-              </p>
-            </Col>
-          </Row>
-        </Col>
-        <Col className="md-3">
-          <Row>
-            <Col className="iconCol">
-              <Image className="iconImg" src={EmailIcon} />
-            </Col>
-            <Col className="textCol">
-              <p>Paketman@gmail.com</p>
-            </Col>
-          </Row>
-        </Col>
-        <Col className="md-3">
-          <Row>
-            <Col className="iconCol">
-              <Image className="iconImg" src={PhoneIcon} />
-            </Col>
-            <Col className="textCol">
-              <p>0531 634 56 60</p>
-            </Col>
-          </Row>
-        </Col>
+        {infosData.map((data, index) => (
+          <InfoCard icon={data.icon} text={data.text} key={index} />
+        ))}
       </Row>
     </Container>
   );
