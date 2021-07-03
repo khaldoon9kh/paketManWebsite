b4714ad31134d65ab4fa107e40419e374abf28c6
khaldoon khaldi <khaldoon9kh9@gmail.com>
2021-07-03 20:35:18 +0300

    change socil icons

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -2,23 +2,28 @@ import React, { useState } from "react";
 import { Row, Col, Image, Container } from "react-bootstrap";
 import "./index.css";
 import PaketManLogo from "../../assests/pics/PaketManWordmark@1X.png";
-import FBIcon from "../../assests/socialMediaIcon/FB.svg";
-import InstaIcon from "../../assests/socialMediaIcon/INSTA.svg";
-import YTIcon from "../../assests/socialMediaIcon/YT.svg";
-import LinkedInIcon from "../../assests/socialMediaIcon/LinkedIn.svg";
-import TikTokIcon from "../../assests/socialMediaIcon/TIKTOK.svg";
+// import FBIcon from "../../assests/socialMediaIcon/FB.svg";
+// import InstaIcon from "../../assests/socialMediaIcon/INSTA.svg";
+// import YTIcon from "../../assests/socialMediaIcon/YT.svg";
+// import LinkedInIcon from "../../assests/socialMediaIcon/LinkedIn.svg";
+// import TikTokIcon from "../../assests/socialMediaIcon/TIKTOK.svg";
 import TMIcon from "../../assests/socialMediaIcon/TD.svg";
+import FB from "../../assests/socialMediaIcon/FB";
+import INSTA from "../../assests/socialMediaIcon/INSTA";
+import LINKEDIN from "../../assests/socialMediaIcon/LINKEDIN";
+import YT from "../../assests/socialMediaIcon/YT";
+import TIKTOK from "../../assests/socialMediaIcon/TIKTOK";
 
 const Footer = () => {
   return (
     <Container fluid className="footerCont" expand="lg">
       <Row>
         <Col className="socialCol">
-          <Image src={FBIcon} />
-          <Image src={InstaIcon} />
-          <Image src={YTIcon} />
-          <Image src={TikTokIcon} />
-          <Image src={LinkedInIcon} />
+          <FB />
+          <INSTA />
+          <YT />
+          <TIKTOK />
+          <LINKEDIN />
           <p>/paketmanapp</p>
         </Col>
         <Col className="logoCol">
